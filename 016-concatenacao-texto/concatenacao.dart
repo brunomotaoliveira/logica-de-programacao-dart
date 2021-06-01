d0872087829b3e5b2457168dbfcf51d5cd55ec4f
brunomotadev <brunomotadev@gmail.com>
2021-06-01 18:42:45 -0300

    concatenação de texto e estrutura de decisão

---
+++ 016-concatenacao-texto/concatenacao.dart
@@ -0,0 +1,10 @@
+main() {
+  //var total = "Hello" + "World";
+
+  var texto1 = "Bruno ";
+  var texto2 = "Mota";
+
+  var total = texto1 + texto2;
+
+  print(total);
+}
---
+++ 017-estrutura-decisao/decisao.dart
@@ -0,0 +1,9 @@
+main() {
+  var isTrue = false; // true ou false
+
+  if (isTrue) {
+    print("Isso é verdadeiro");
+  } else {
+    print("Não é verdadeiro");
+  }
+}
