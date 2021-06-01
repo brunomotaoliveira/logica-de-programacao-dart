0aa733a7d6670863bafc957bfdd950f9aab2a4d1
brunomotadev <brunomotadev@gmail.com>
2021-06-01 20:04:40 -0300

    primeiro algoritmo

---
+++ 018-primeiro-algoritmo/primeiro-algoritmo.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+main() {
+  print("======== Digite uma idade ========");
+  var input = stdin.readLineSync();
+  var idade = int.parse(input!);
+
+  if (idade >= 18) {
+    print("maior de idade");
+  } else {
+    print("menor de idade");
+  }
+}
