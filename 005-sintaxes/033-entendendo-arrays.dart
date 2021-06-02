4e3aa5588971fa344b20cb74aed8ab6569486581
brunomotadev <brunomotadev@gmail.com>
2021-06-02 18:22:44 -0300

    Entendendo arrays

---
+++ 005-sintaxes/033-entendendo-arrays.dart
@@ -0,0 +1,9 @@
+main() {
+  var nomes = ["Bruno Mota", "Jéssic Silva", "Rosângela das Graças", "Albert"];
+
+  nomes.add("João Paulo");
+  nomes.add("Elisabeth");
+
+  print(nomes.length);
+  print(nomes);
+}
