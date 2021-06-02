062f97afe1525bf783d472dd1a2538cdfaab4db3
brunomotadev <brunomotadev@gmail.com>
2021-06-02 14:59:36 -0300

    funções com retorno - Dart

---
+++ 004-estrutura-de-programa/funcoes-com-retorno.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+main() {
+  calculoImc();
+}
+
+calculoImc() {
+  print("=== Digite seu peso");
+  String? textPeso = stdin.readLineSync();
+  int peso = int.parse(textPeso!);
+
+  print("=== Digite sua altura");
+  String? textAltura = stdin.readLineSync();
+  double altura = double.parse(textAltura!);
+
+  double imc = calcImcExpressao(peso, altura);
+  imprimirResultado(imc);
+}
+
+double calcImcExpressao(int peso, double altura) {
+  return peso / (altura * altura);
+}
+
+imprimirResultado(double imc) {
+  print("================================");
+
+  if (imc < 18.5) {
+    print("Abaixo do peso");
+  } else if (imc > 18.5 && imc < 24.9) {
+    print("Peso normal");
+  } else if (imc > 25 && imc < 29.9) {
+    print("Sobrepeso");
+  } else if (imc > 30 && imc < 34.9) {
+    print("Obesidade grau 1");
+  } else if (imc > 35 && imc < 39.9) {
+    print("Obesidade grau 2");
+  } else {
+    print("Obesidade grau 3");
+  }
+}
