786b743a5e76b061515b864a8435c26af908c3bb
brunomotadev <brunomotadev@gmail.com>
2021-06-03 16:29:25 -0300

    o que são maps?

---
+++ 007-maps/maps.dart
@@ -0,0 +1,13 @@
+
+//maps é uma forma de representar os dados
+main() {
+  var maps = {
+    "nome": "Bruno Mota",
+    "idade": 29,
+    "cidade": "Sao Paulo",
+    "estado": "Sao Paulo"
+  };
+
+  
+  print(maps["nome"]);
+}
